0b4b78749d4c5002d0f98bb4df6f72010c7a7269
augusstt06 <augusstt06@gmail.com>
2023-02-01 19:34:17 +0900

    Comments

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+// html Body 부분
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
