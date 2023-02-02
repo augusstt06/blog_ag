83f84717f8c84a495dc25155a5c2f77a7b0b8afe
augusstt06 <augusstt06@gmail.com>
2023-02-02 18:27:13 +0900

    Add Custom Hook

---
+++ frontend/src/hook/useDetectClose.ts
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+const useDetectClose = (elem: any, initialState: any) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  useEffect(() => {
+    const onClick = (e: any) => {
+      if (elem.current !== null && !elem.current.contains(e.target)) {
+        setIsOpen(!isOpen);
+      }
+    };
+    if (isOpen) {
+      window.addEventListener("click", onClick);
+    }
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, [isOpen, elem]);
+  return [isOpen, setIsOpen];
+};
+
+export default useDetectClose;
