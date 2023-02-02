3ca10ede17447e8c97e5cfa8e602e5b6ba84ace1
augusstt06 <augusstt06@gmail.com>
2023-02-02 23:59:03 +0900

    header component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,23 @@
+import Nav from "./nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Header() {
+  return (
+    <div>
+      <div>로고</div>
+      <div>검색창 - 컴포넌트</div>
+      <Nav />
+      <style jsx>
+        {`
+          div {
+            display: flex;
+            justify-content: space-between;
+            padding: 5px 20px;
+            align-items: center;
+            height: 4rem;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
