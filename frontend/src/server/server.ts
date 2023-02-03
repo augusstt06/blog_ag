951b50ec1e34d7c171bd4edef524e1dc9c33e0c7
augusstt06 <augusstt06@gmail.com>
2023-02-03 22:51:27 +0900

    Create Server folder

---
+++ frontend/src/server/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import createServer from "next/dist/server/next";
+import next from "next";
+
+const dev = process.env.NODE_ENV === "development";
+const port = 3000;
+const app = next({ dev, port });
+
+app.prepare().then(() => {
+  const server = express();
+});
