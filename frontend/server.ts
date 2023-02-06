eafb4306888924338b503d71733b051f28310ccd
augusstt06 <augusstt06@gmail.com>
2023-02-06 18:38:39 +0900

    Execution Next js Custom Server

---
+++ frontend/server.ts
@@ -0,0 +1,24 @@
+const express = require("express");
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.get("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(3001, (err?: any) => {
+      if (err) throw err;
+      console.log("Server Ready");
+    });
+  })
+  .catch((ex: any) => {
+    console.error(ex);
+    process.exit(1);
+  });
+export {};
