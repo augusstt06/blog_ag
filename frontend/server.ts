c618bbe6b3866e86eecb90a504aaa655b0732906
augusstt06 <augusstt06@gmail.com>
2023-02-09 23:45:08 +0900

    Server : Post request test

--- frontend/server.ts
+++ frontend/server.ts
@@ -5,29 +5,28 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+app.prepare().then(() => {
+  const server = express();
 
-    server.get("/post", (req: Request, res: Response) => {
-      console.log("포스팅 페이지");
-    });
-    server.post("/post", (req: Request, res: Response) => {
-      console.log("포스트 요청");
-      return res;
-    });
-    server.get("*", (req: Request, res: Response) => {
-      return handle(req, res);
-    });
-
-    server.listen(3000, (err?: any) => {
-      if (err) throw err;
-      console.log("Server Ready");
-    });
-  })
-  .catch((ex: any) => {
-    console.error(ex);
-    process.exit(1);
+  server.get("/post", (req: Request, res: Response) => {
+    console.log("포스팅 페이지");
   });
+  server.post("/post", (req: Request, res: Response) => {
+    console.log("포스트 요청");
+    return res;
+  });
+  server.get("*", (req: Request, res: Response) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err?: any) => {
+    if (err) throw err;
+    console.log("Server Ready");
+  });
+});
+// .catch((ex: any) => {
+//   console.error(ex);
+//   process.exit(1);
+// });
+
 export {};
