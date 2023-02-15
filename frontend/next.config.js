b8e109ca04b961858d0ba033a30543c54436ec31
augusstt06 <augusstt06@gmail.com>
2023-02-16 00:34:44 +0900

    New branch

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   env: {
     NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
   },
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 };
 
 module.exports = nextConfig;
