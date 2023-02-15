067eddc4b0e1e6ea7873f4db8538ebac46617b4f
augusstt06 <augusstt06@gmail.com>
2023-02-16 00:34:22 +0900

    New branch

--- frontend/src/db/dbConnect.ts
+++ frontend/src/db/dbConnect.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
 export async function handler(req: any, res: any) {
-  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
-  const database = client.db();
+  //   const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
+  //   const database = client.db();
 }
