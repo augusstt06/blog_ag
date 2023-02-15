b13da407af633d098b283b1fa25a641265171494
augusstt06 <augusstt06@gmail.com>
2023-02-16 00:31:56 +0900

    branch stash

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   env: {
     NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
   },
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 };
