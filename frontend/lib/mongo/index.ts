db96c167c580c0c4579941b392425fde765c4f90
augusstt06 <augusstt06@gmail.com>
2023-02-19 00:05:26 +0900

    Successfully fetching data from Mongo DB

--- frontend/lib/mongo/index.ts
+++ frontend/lib/mongo/index.ts
@@ -1,49 +1,70 @@
-// import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 
-// const URI = process.env.NEXT_PUBLIC_MONGODB_URI;
-// const options = {};
+const URI = process.env.NEXT_PUBLIC_MONGODB_URI;
+const options = {};
 
-// if (!URI) throw new Error("Please add your MOngo DB URI to .env.local");
+if (!URI) throw new Error("Please add your MOngo DB URI to .env.local");
 
-// let client = new MongoClient(URI, options);
-// let clientPromise: Promise<MongoClient>;
-
-// if (process.env.NODE_ENV !== "production") {
-//   let globalWithMongoClientPromise = global as typeof globalThis & {
-//     _mongoClientPromise: Promise<MongoClient>;
-//   };
-
+let client = new MongoClient(URI, options);
+let clientPromise: Promise<MongoClient>;
+// const globalWithMongoClientPromise = global as typeof globalThis & {
+//   _mongoClientPromise: Promise<MongoClient>;
+// };
+// if (process.env.NODE_ENV === "development") {
+//   // In development mode, use a global variable so that the value
+//   // is preserved across module reloads caused by HMR (hot module replacement).
 //   if (!globalWithMongoClientPromise._mongoClientPromise) {
-//     client = new MongoClient(URI);
+//     client = new MongoClient(URI!, options);
 //     globalWithMongoClientPromise._mongoClientPromise = client.connect();
 //   }
-
-//   clientPromise = globalWithMongoClientPromise._mongoClientPromise;
+//   clientPromise =
+//     globalWithMongoClientPromise._mongoClientPromise as Promise<MongoClient>;
 // } else {
-//   client = new MongoClient(URI);
+//   // In production mode, it's best to not use a global variable.
+//   client = new MongoClient(URI!, options);
+//   console.log("Start to connect...");
 //   clientPromise = client.connect();
 // }
-
+// // Export a module-scoped MongoClient promise. By doing this in a
+// // separate module, the client can be shared across functions.
 // export default clientPromise;
 
-//
-import * as mongo from "mongodb";
-import { NextApiRequest } from "next";
+if (process.env.NODE_ENV !== "production") {
+  let globalWithMongoClientPromise = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>;
+  };
 
-const MONGO_URI = process.env.NEXT_PUBLIC_MONGODB_URI;
+  if (!globalWithMongoClientPromise._mongoClientPromise) {
+    client = new MongoClient(URI);
+    globalWithMongoClientPromise._mongoClientPromise = client.connect();
+  }
 
-interface Database {
-  client?: mongo.MongoClient;
+  clientPromise = globalWithMongoClientPromise._mongoClientPromise;
+} else {
+  console.log("Start to connect....");
+  client = new MongoClient(URI);
+  clientPromise = client.connect();
 }
-export interface DB_Request extends NextApiRequest {
-  db: Database;
-}
 
-const connectDB = async function () {
-  console.log(`Start Connecting to Mongo DB || ${MONGO_URI}`);
-  const mongoClient: mongo.MongoClient = new mongo.MongoClient(MONGO_URI, {
-    connectTimeoutMS: 5000,
-  });
-  const client = await mongoClient.connect();
-  // const database:mongo.Db = client.db(process.env.)
-};
+export default clientPromise;
+
+// import * as mongo from "mongodb";
+// import { NextApiRequest } from "next";
+
+// const MONGO_URI = process.env.NEXT_PUBLIC_MONGODB_URI;
+
+// interface Database {
+//   client?: mongo.MongoClient;
+// }
+// export interface DB_Request extends NextApiRequest {
+//   db: Database;
+// }
+
+// const connectDB = async function () {
+//   console.log(`Start Connecting to Mongo DB || ${MONGO_URI}`);
+//   const mongoClient: mongo.MongoClient = new mongo.MongoClient(MONGO_URI, {
+//     connectTimeoutMS: 5000,
+//   });
+//   const client = await mongoClient.connect();
+//   // const database:mongo.Db = client.db(process.env.)
+// };
--- frontend/src/pages/api/movies.ts
+++ frontend/src/pages/api/movies.ts
@@ -1,19 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getMovies } from "../../../lib/mongo/movies";
+// // import { getMovies } from "../../../lib/mongo/movies";
+
+// export default async function hanlder(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "GET") {
+//     try {
+//       const { movies, error } = await getMovies();
+//       if (error) throw new Error(error);
+//       return res.status(200).json({ movies });
+//     } catch (error) {
+//       return res.status(500).json({ error });
+//     }
+//   }
+//   res.setHeader("Allow", ["GET"]);
+//   res.status(425).end(`Method ${req.method} is not allowed`);
+// }
+
+import clientPromise from "../../../lib/mongo";
 
 export default async function hanlder(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    try {
-      const { movies, error } = await getMovies();
-      if (error) throw new Error(error);
-      return res.status(200).json({ movies });
-    } catch (error) {
-      return res.status(500).json({ error });
-    }
+  try {
+    const client = await clientPromise;
+    const db = client.db("sample_mflix");
+    const movies = await db
+      .collection("movies")
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(10)
+      .toArray();
+    res.json(movies);
+  } catch (err) {
+    console.error(err);
   }
-  res.setHeader("Allow", ["GET"]);
-  res.status(425).end(`Method ${req.method} is not allowed`);
 }
