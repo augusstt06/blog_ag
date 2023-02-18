ec3ef301387f4eef8ff469a7d6d7cd7e790ca801
augusstt06 <augusstt06@gmail.com>
2023-02-19 00:15:14 +0900

    Successfully fetching data from Mongo DB

--- frontend/lib/mongo/index.ts
+++ frontend/lib/mongo/index.ts
@@ -7,27 +7,6 @@ if (!URI) throw new Error("Please add your MOngo DB URI to .env.local");
 
 let client = new MongoClient(URI, options);
 let clientPromise: Promise<MongoClient>;
-// const globalWithMongoClientPromise = global as typeof globalThis & {
-//   _mongoClientPromise: Promise<MongoClient>;
-// };
-// if (process.env.NODE_ENV === "development") {
-//   // In development mode, use a global variable so that the value
-//   // is preserved across module reloads caused by HMR (hot module replacement).
-//   if (!globalWithMongoClientPromise._mongoClientPromise) {
-//     client = new MongoClient(URI!, options);
-//     globalWithMongoClientPromise._mongoClientPromise = client.connect();
-//   }
-//   clientPromise =
-//     globalWithMongoClientPromise._mongoClientPromise as Promise<MongoClient>;
-// } else {
-//   // In production mode, it's best to not use a global variable.
-//   client = new MongoClient(URI!, options);
-//   console.log("Start to connect...");
-//   clientPromise = client.connect();
-// }
-// // Export a module-scoped MongoClient promise. By doing this in a
-// // separate module, the client can be shared across functions.
-// export default clientPromise;
 
 if (process.env.NODE_ENV !== "production") {
   let globalWithMongoClientPromise = global as typeof globalThis & {
@@ -47,24 +26,3 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 export default clientPromise;
-
-// import * as mongo from "mongodb";
-// import { NextApiRequest } from "next";
-
-// const MONGO_URI = process.env.NEXT_PUBLIC_MONGODB_URI;
-
-// interface Database {
-//   client?: mongo.MongoClient;
-// }
-// export interface DB_Request extends NextApiRequest {
-//   db: Database;
-// }
-
-// const connectDB = async function () {
-//   console.log(`Start Connecting to Mongo DB || ${MONGO_URI}`);
-//   const mongoClient: mongo.MongoClient = new mongo.MongoClient(MONGO_URI, {
-//     connectTimeoutMS: 5000,
-//   });
-//   const client = await mongoClient.connect();
-//   // const database:mongo.Db = client.db(process.env.)
-// };
