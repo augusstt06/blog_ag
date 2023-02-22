0a3c8e84e3dbc1bb301519e867c675656bc1d9eb
augusstt06 <augusstt06@gmail.com>
2023-02-22 15:21:20 +0900

    Handling input values using ref

--- frontend/src/pages/post/index.tsx
+++ frontend/src/pages/post/index.tsx
@@ -1,34 +1,39 @@
 // 글 등록 페이지
-import React, { useState, useEffect } from "react";
+import React, { useRef } from "react";
 import axios from "axios";
 
 export default function Post() {
-  const [write, setWrite] = useState<string>("");
-  const [post, setPost] = useState<object>({ post: "" });
+  const main = useRef(null);
+  const title = useRef(null);
 
-  const posting = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setWrite(e.target.value);
-    setPost({ post: write });
-  };
-  console.log(post, "요청 보낼 객체");
   const registerPost = async () => {
     try {
-      const res = await axios.post("/api/post", post, {
-        withCredentials: true,
-      });
-      console.log(res.data);
+      const res = await axios.post(
+        "/api/post",
+        {
+          title: title.current.value,
+          post: main.current.value,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      console.log("response:", res.data);
+
+      title.current.value = null;
+      main.current.value = null;
     } catch (err) {
       console.log(err);
     }
   };
+
   return (
     <>
-      <div>등록</div>
-      <textarea
-        placeholder="게시글을 입력하세요"
-        value={write}
-        onChange={posting}
-      />
+      <div>제목</div>
+      <textarea placeholder="제목을 입력하세요" ref={title} />
+      <div>본문</div>
+      <textarea placeholder="게시글을 입력하세요" ref={main} />
       <button onClick={registerPost}>등록</button>
     </>
   );
