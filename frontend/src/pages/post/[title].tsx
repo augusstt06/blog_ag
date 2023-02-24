0fb1545a5b984597a4ee9f019cdaf684afdfd020
augusstt06 <augusstt06@gmail.com>
2023-02-24 23:11:08 +0900

    Connect with MongoDB data to render detailed screen

--- frontend/src/pages/post/[title].tsx
+++ frontend/src/pages/post/[title].tsx
@@ -2,7 +2,8 @@ import React from "react";
 import axios from "axios";
 import { GetServerSideProps } from "next";
 
-// 한글 제목일때 생각해서 바꾸기
+// 한글 제목일때 생각해서 바꾸기 => id 변수 생성?
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const query = context.query;
   const res = await axios.get(`http://localhost:3000/api/post/${query.title}`, {
