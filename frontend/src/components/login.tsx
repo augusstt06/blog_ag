c01a8a938a8b6e0079d4ff477e1ca74be4569414
augusstt06 <augusstt06@gmail.com>
2023-02-27 16:39:57 +0900

    Added automatic logout function (10 minutes)

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,9 +1,29 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useCallback, useEffect } from "react";
 
 export default function Login() {
   const { data } = useSession();
-  // console.log("로그인 데이터", data);
 
+  const autoLogout = () => {
+    if (data !== null && data !== undefined) {
+      setTimeout(() => {
+        const out = confirm(
+          "곧 로그인이 해제됩니다. 로그인을 유지하려면 확인 버튼을 눌러주세요"
+        );
+        if (out) {
+          alert("로그인이 갱신 되었습니다.");
+          console.log("유지", data);
+        } else {
+          signOut();
+          console.log("로그아웃", data);
+          ("로그아웃 되었습니다.");
+        }
+      }, 100000);
+    }
+  };
+  useEffect(() => {
+    autoLogout();
+  });
   return (
     <div>
       {data?.user ? (
