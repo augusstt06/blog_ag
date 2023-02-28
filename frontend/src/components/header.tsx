de1a8b60e7eaf3d9dea1c5d6b2501039065eb25e
augusstt06 <augusstt06@gmail.com>
2023-02-28 17:52:07 +0900

    Integrate nav.tsx and header.tsx

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,33 +1,65 @@
-import Nav from "./nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
 import Login from "./login";
 
+import { useSession } from "next-auth/react";
+
 export default function Header() {
+  const email = process.env.NEXT_PUBLIC_EMAIL;
+  const { status, data } = useSession();
+  const gitUrl = process.env.NEXT_PUBLIC_GIT;
   return (
-    <div>
-      <div>
-        <Link href="/">로고</Link>
-        <Login />
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#!">
+          <Login />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page">
+                <Link className="nav-link text-white" href="/">
+                  Home
+                </Link>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                <Link className="nav-link text-white" href={gitUrl}>
+                  Git
+                </Link>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                <Link className="nav-link text-white" href="/">
+                  Contact
+                </Link>
+              </a>
+            </li>
+            {status === "authenticated" && data.user.email === email ? (
+              <li className="nav-item">
+                <a className="nav-link">
+                  <Link className="nav-link text-white" href="/post">
+                    Post
+                  </Link>
+                </a>
+              </li>
+            ) : null}
+          </ul>
+        </div>
       </div>
-      <div>
-        <a>검색창 - 컴포넌트</a>
-      </div>
-      <div>
-        <Nav />
-      </div>
-      <style jsx>
-        {`
-          div {
-            display: flex;
-            justify-content: space-between;
-            padding: 5px 20px;
-            align-items: center;
-            height: 4rem;
-            background: gray;
-          }
-        `}
-      </style>
-    </div>
+    </nav>
   );
 }
