34c2692aa1b0451ba820bca063b26b45b24e5b1d
augusstt06 <augusstt06@gmail.com>
2023-03-03 14:47:59 +0900

    Get a list of articles in db by connecting to api

--- frontend/src/pages/post/list.tsx
+++ frontend/src/pages/post/list.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import axios from "axios";
+import { GetServerSideProps } from "next";
 
-export default function PostList() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await axios.get(`http://localhost:3000/api/post`, {
+    withCredentials: true,
+  });
+  return { props: { data: res.data } };
+};
+
+export default function PostList({ data }: any) {
+  console.log("서버사이드props", data);
   return <>글 목록</>;
 }
