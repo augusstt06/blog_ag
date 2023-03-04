fc206ab146e01c0560e397b90dcb2753f6e00268
augusstt06 <augusstt06@gmail.com>
2023-03-04 17:20:43 +0900

    Css test

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -14,24 +14,33 @@ export default function Header() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <a className="navbar-brand">
+      <nav
+        className="navbar navbar-expand-lg navbar-light fixed-top"
+        id="mainNav"
+      >
+        <div className="container px-4 px-lg-5">
+          <a className="navbar-brand" href="#page-top">
             <Login />
           </a>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler navbar-toggler-right"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            Menu
+            <i className="fas fa-bars"></i>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  About
+                </a>
+              </li>
               <li className="nav-item">
                 <Link className="nav-link" href="/">
                   Home
@@ -42,6 +51,7 @@ export default function Header() {
                   Git
                 </Link>
               </li>
+              {/* <li className="nav-item"><a className="nav-link" href="#signup">Contact</a></li> */}
               {currentUrl !== "/post/list" ? (
                 <>
                   <li className="nav-item">
@@ -78,6 +88,70 @@ export default function Header() {
           </div>
         </div>
       </nav>
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand">
+            <Login />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" href="/">
+                  Home
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" href={gitUrl}>
+                  Git
+                </Link>
+              </li>
+              {currentUrl !== "/post/list" ? (
+                <>
+                  <li classNameName="nav-item">
+                    <Link classNameName="nav-link" href="/post/list">
+                      Articles
+                    </Link>
+                  </li>{" "}
+                </>
+              ) : (
+                <>
+                  <li classNameName="nav-item">
+                    <Link classNameName="nav-link" href="/post/list">
+                      Programming
+                    </Link>
+                  </li>
+                  <li classNameName="nav-item">
+                    <Link classNameName="nav-link" href="/post/list">
+                      LostArk
+                    </Link>
+                  </li>
+                </>
+              )}
+
+              {status === "authenticated" && data.user.email === email ? (
+                <li classNameName="nav-item">
+                  <Link classNameName="nav-link" href="/post">
+                    Posting
+                  </Link>
+                </li>
+              ) : (
+                <></>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav> */}
     </>
   );
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { wrapper } from "@/store";
 import { SessionProvider } from "next-auth/react";
 import Layout from "@/layout";
+import "../styles/nav.css";
 
 // html Body 부분
 
