9fdcee14437e92f5bc6bb7115e3cb34791b03c2e
augusstt06 <augusstt06@gmail.com>
2023-03-04 19:55:31 +0900

    Main page composition

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,9 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/reducers/rootReducer";
 import Image from "next/image";
 import kuku from "../asset/kuku.jpg";
+import { FaReact } from "react-icons/fa";
+import { RiVuejsFill } from "react-icons/ri";
+import { SiTypescript } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
 
-// 환경변수 설정하기
-// 메인 화면은 심플한 페이지네임이랑 애니메이션으로 구성하고
 export async function getStaticProps() {
   const backgroundUrl = "https://source.unsplash.com/wfh8dDlNFOk/1600x900";
 
@@ -13,24 +17,135 @@ export async function getStaticProps() {
   };
 }
 export default function Home({ backgroundUrl }: any) {
-  const dispatch = useDispatch();
-  const { value } = useSelector((state: RootState) => state.counterReducer);
+  const language_stack = [
+    {
+      image: <DiJavascript1 size={80} />,
+      name: "Java Script",
+      introduction: (
+        <p>
+          웹 페이지에서 복잡한 기능을 구현할 수 있도록 하는
+          <br /> <em>객체(object) 기반</em>의 스크립트 언어
+        </p>
+      ),
+    },
+    {
+      image: <SiTypescript size={80} />,
+      name: "Type Script",
+      introduction: (
+        <p>
+          자바스크립트(JavaScript)를 기반으로
+          <br />
+          <em>정적 타입 문법</em>을 추가한 프로그래밍 언어
+        </p>
+      ),
+    },
+    {
+      image: <FaPython size={80} />,
+      name: "Python",
+      introduction: (
+        <p>
+          소프트웨어 개발, 데이터 과학, 기계 학습에
+          <br /> 널리 사용되는 프로그래밍 언어
+        </p>
+      ),
+    },
+  ];
+  const framework_stack = [
+    {
+      image: <FaReact size={80} />,
+      name: "React",
+      introduction: (
+        <p>
+          웹 페이지 개발을 위해 <br />
+          컴포넌트를 기반으로 한 <em>SPA 라이브러리</em>
+        </p>
+      ),
+    },
+    {
+      image: <SiNextdotjs size={80} />,
+      name: "Next Js",
+      introduction: (
+        <p>
+          React를 기반으로 한 <em>SSR 프레임워크</em>
+        </p>
+      ),
+    },
+    {
+      image: <RiVuejsFill size={80} />,
+      name: "Vue",
+      introduction: (
+        <p>
+          웹 페이지 개발을 위한
+          <br /> <em>SPA 프레임워크</em>
+        </p>
+      ),
+    },
+  ];
+
   return (
     <>
       <header className="py-5 bg-image-full">
-        <div className="text-center my-5">
+        {/* <div className="text-center my-5"> */}
+        <div className="text-center">
           <Image
             className="img-fluid rounded-circle mb-4"
             src={kuku}
             alt="쿠크 안뇽"
-            width="150"
-            height="150"
+            width={100}
+            height={100}
           />
-          <h1 className="text-white fs-3 fw-bolder">augusstt06의 블로그</h1>
-          <p className="text-white-50 mb-0">프로그래밍, 취미생활</p>
+          <h1 className="text-white fs-3 fw-bolder">
+            <p>augusstt06&apos;s Blog</p>
+          </h1>
+          <p className="text-white-50 mb-0">개발과 취미생활</p>
         </div>
       </header>
+      {/* -------태크 스택 소개 ------*/}
+      <section className="page-section" id="services">
+        <div className="container px-4 px-lg-5">
+          <h1 className="text-center mt-0">
+            <em>Tech Stacks</em>
+          </h1>
 
+          <hr className="divider" />
+          <h3 className="text-center mt-0">
+            <em>FrameWork</em>
+          </h3>
+          <div className="row gx-4 gx-lg-5 justify-content-center">
+            {framework_stack.map((data) => (
+              <div className="col-lg-3 col-md-6 text-center" key={data.name}>
+                <div className="mt-5">
+                  <div className="mb-2">
+                    <i className="bi-gem fs-1 text-primary"></i>
+                  </div>
+                  {data.image}
+                  <h3 className="h4 mb-2">{data.name}</h3>
+                  <p className="text-muted mb-0">{data.introduction}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <h3 className="text-center mt-0">
+            <em>Language</em>
+          </h3>
+          <div className="row gx-4 gx-lg-5 justify-content-center">
+            {language_stack.map((data) => (
+              <div className="col-lg-3 col-md-6 text-center" key={data.name}>
+                <div className="mt-5">
+                  <div className="mb-2">
+                    <i className="bi-gem fs-1 text-primary"></i>
+                  </div>
+                  {data.image}
+                  <h3 className="h4 mb-2">{data.name}</h3>
+                  <p className="text-muted mb-0">{data.introduction}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* ------------------------- */}
       <section className="py-5">
         <div className="container my-5">
           <div className="row justify-content-center">
@@ -78,7 +193,7 @@ export default function Home({ backgroundUrl }: any) {
       <footer className="py-5 bg-dark">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2022
+            Copyright &copy; March 2023
           </p>
         </div>
       </footer>
@@ -86,6 +201,8 @@ export default function Home({ backgroundUrl }: any) {
         {`
           header {
             background-image: url(${backgroundUrl});
+            height: 40vh;
+            margin-bottom: 10px;
           }
         `}
       </style>
