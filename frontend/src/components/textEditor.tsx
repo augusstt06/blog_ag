c02fe88e160dc45753fc1fe87ca8f698e741d825
augusstt06 <augusstt06@gmail.com>
2023-03-09 15:50:49 +0900

    Quill Image Handler

--- frontend/src/components/textEditor.tsx
+++ frontend/src/components/textEditor.tsx
@@ -15,23 +15,6 @@ hljs.registerLanguage("python", python);
 hljs.registerLanguage("typescript", typescript);
 hljs.registerLanguage("c", c);
 
-const imageHandler = () => {
-  const input = document.createElement("input");
-  input.setAttribute("type", "file");
-  input.setAttribute("accept", "image/*");
-  input.click();
-
-  input.addEventListener("change", async () => {
-    const file = input.files[0];
-    const formData = new FormData();
-    formData.append("img", file);
-    console.log(formData, "?");
-    // try{
-    //   const result = await axios.post()
-    // }
-  });
-};
-
 const modules = {
   syntax: {
     highlight: (text: any) => hljs.highlightAuto(text).value,
@@ -49,9 +32,6 @@ const modules = {
     ["link", "image", "video", "code-block"],
     ["clean"],
   ],
-  handlers: {
-    image: imageHandler,
-  },
 };
 
 const formats = [
